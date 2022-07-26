7c7c69825b79112efec87c839059e8becf9ee498
Saipranav Kalapala <saipranav.kalapala@canfieldsci.com>
2022-07-26 10:32:33 -0400

    updated signing

--- Bullseye-test-app/App/BullseyeApp.swift
+++ Bullseye-test-app/App/BullseyeApp.swift
@@ -14,7 +14,7 @@ import AppCenterCrashes
 struct BullseyeApp: App {
     
     init() {
-        //AppCenter.start(withAppSecret: ProcessInfo.processInfo.environment["APP_SECRET"]!, services: [Analytics.self, Crashes.self])
+        AppCenter.start(withAppSecret: "08a14c2e-61e8-4c88-831f-b32b4c922332", services: [Analytics.self, Crashes.self])
         
     }
     
