2cf33d093345f7833fd81db249e0b80a5f6e8ea1
Saipranav Kalapala <saipranav.kalapala@canfieldsci.com>
2022-08-10 11:37:07 -0400

    updated failing test

--- Bullseye-test-appUITests/Bullseye-test-appUITests.swift
+++ Bullseye-test-appUITests/Bullseye-test-appUITests.swift
@@ -79,7 +79,7 @@ class BullseyeUITests: XCTestCase {
         print(resultMessage.label)
         print(components[components.count - 2])
         
-        //XCTAssertTrue(false)
+        XCTAssertTrue(false)
     }
 
 //    func testLaunchPerformance() throws {
